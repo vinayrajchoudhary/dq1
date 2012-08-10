7d2ec85f67341889b95cc4e2edabbbbed13b889b
vinayrajchoudhary <vinayrajchoudhary@gmail.com>
2012-08-11 01:50:51 +0530

    heroku

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,7 @@ module Blog
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-  end
+config.assets.initialize_on_precompile=false
+rake assets:precompile  
+end
 end
